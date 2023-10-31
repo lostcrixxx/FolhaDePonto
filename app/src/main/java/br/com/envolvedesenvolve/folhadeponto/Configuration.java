bfec5330d36ecc49162294aeb325323dc5d72f95
Cristiano M <devcristianomorais@gmail.com>
2023-10-30 23:43:03 -0300

    added configuration file

---
+++ app/src/main/java/br/com/envolvedesenvolve/folhadeponto/Configuration.java
@@ -0,0 +1,15 @@
+package br.com.envolvedesenvolve.folhadeponto;
+
+public class Configuration {
+
+    public static final String APP_SCRIPT_WEB_APP_URL = Config.KEY;
+    public static final String ADD_USER_URL = APP_SCRIPT_WEB_APP_URL;
+    public static final String LIST_USER_URL = APP_SCRIPT_WEB_APP_URL+"?action=readAll";
+
+    public static final String KEY_ID = "uId";
+    public static final String KEY_NAME = "uName";
+    public static final String KEY_IMAGE = "uImage";
+    public  static final String KEY_ACTION = "action";
+
+    public static final String KEY_USERS = "records";
+}
